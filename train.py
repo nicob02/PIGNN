d1539984e1972d827efd4975fc9f068b16ee28f5
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-08-16 12:36:27 -0400

    Update train.py

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ setattr(train_config, 'graph', graph)
 setattr(train_config, 'model', model)
 setattr(train_config, 'optimizer', optimizer)
 setattr(train_config, 'train_steps', 60)    # 1 minute total simulation
-setattr(train_config, 'epchoes', 50)
+setattr(train_config, 'epchoes', 500)
 setattr(train_config, 'NodeTypesRef', ElectrodeMesh.node_type_ref) 
 setattr(train_config, 'step_times', 1)
 #setattr(train_config, 'name', func_name)
