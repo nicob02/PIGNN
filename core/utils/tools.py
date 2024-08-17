24b6a211e2634506e2c969fcdf51bf8be90cb8b0
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-08-17 16:46:12 -0400

    Update tools.py

--- core/utils/tools.py
+++ core/utils/tools.py
@@ -69,7 +69,7 @@ def modelTrainer(config):
             value_last = graph.x.detach().clone()
             volt_last = graph.x[:,1:2]
             temp_last = graph.x[:,0:1]
-            condition = (volt_last >= 4) & (temp_last < 314)
+            condition = (volt_last >= 3) & (temp_last < 314)
             temp_last[condition] = 315
 
             
