830095faf91b31fe840366b894ece04f6b23dd2d
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-10-23 19:59:16 -0400

    Add files via upload

--- NM.py
+++ NM.py
@@ -125,7 +125,7 @@ bc_elec_V = DirichletBC(ET.sub(0), Constant(18), electrode_surface) # Volt = 18
 sigma_electrode = Constant(1e8)  # Electrode conductivity (S/m)
 sigma_liver = Constant(0.33)     # Liver conductivity (S/m)
 
-sigma = 0.33*(1 + 0.02*(Te0-309))
+sigma = 5*(1 + 0.02*(Te0-309))
 
 F = ((-sigma)*(inner(grad(Phi),grad(Phi_test))))*dx             # Voltage residual
 
