0af9cbfdd0fb8de0cac11c61c2544e76261e7536
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-10-23 18:40:39 -0400

    Add files via upload

--- NM.py
+++ NM.py
@@ -100,32 +100,6 @@ dt = Expression('dtvalue', dtvalue = 0.1, degree=1)
 
 x =  SpatialCoordinate(Omega.mesh)
 
-num_outer_boundary = 0
-num_electrode_surface = 0
-
-# Loop over all mesh nodes
-for point in Omega.pos:
-    
-    if outer_boundary(point):
-        num_outer_boundary += 1
-    if electrode_surface(point):
-        print(f"Node near electrode surface: {point}")
-        num_electrode_surface += 1
-
-# Print the results
-print(f"Number of nodes on the outer boundary: {num_outer_boundary}")
-print(f"Number of nodes on the electrode surface: {num_electrode_surface}")
-
-# Example: Check if a specific point is on the boundary
-point = (1.0, 0.5)  # Numerical point
-bc1 = outer_boundary(point)  # `on_boundary` is True because we are checking at the boundary
-print("bc1:", bc1)
-
-# Example: Check if a specific point is on the boundary
-point = (0.49, 0.5)  # Numerical point
-bc2 = electrode_surface(point)  # `on_boundary` is True because we are checking at the boundary
-print("bc2:", bc2)
-
 bc_bound_V = DirichletBC(ET.sub(0), Constant(0), outer_boundary)  # Volt = 0 at ground
 bc_Temp = DirichletBC(ET.sub(1), Constant(310), outer_boundary)     # Temp = 310 at ground
 bc_elec_V = DirichletBC(ET.sub(0), Constant(18), electrode_surface) # Volt = 18
@@ -145,7 +119,7 @@ bcs = [bc_bound_V, bc_elec_V, bc_Temp]
 problem = Problem(J, F, bcs)   
 custom_solver = CustomSolver()
 
-custom_solver.parameters["relative_tolerance"] = 1e-3
+custom_solver.parameters["relative_tolerance"] = 1e-6  
 custom_solver.parameters["maximum_iterations"] = 30
 
 # Create VTK files for visualization output
