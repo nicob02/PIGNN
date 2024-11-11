e284cf85aa4e88a0e5e50a0e02e5ad02a18c552a
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-11-10 23:19:51 -0500

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import os
 
 
 
-delta_t = 0.1 # Mess around with this
+delta_t = 0.2 # Mess around with this
 
 #func_name = 'rfa'
 out_ndim = 2
@@ -33,7 +33,7 @@ model = msgPassing(message_passing_num=2, node_input_size=3+out_ndim,
 model.load_model(ckptpath)
 model.to(device)
 model.eval()
-test_steps = 700
+test_steps = 30
 
 test_config = parse_config()
 
