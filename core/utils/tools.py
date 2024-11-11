2ff5f6b12a10423fd473362e875059639feb4555
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-11-11 00:09:38 -0500

    Update tools.py

--- core/utils/tools.py
+++ core/utils/tools.py
@@ -94,8 +94,8 @@ def modelTrainer(config):
             loss = config.pde(graph, values_last=value_last, values_this=predicted)
             if torch.isnan(loss).any():
                 print(f"Warning: NaN detected in pde_loss at step {step}")
-            #loss[on_boundary] = 0
-            #loss[on_electrode] = 0
+            loss[on_boundary] = 0
+            loss[on_electrode] = 0
             #loss_scalar = torch.norm(pde_loss)/pde_loss.numel()
             #loss_scalar = torch.sum(pde_loss)/pde_loss.numel()
          
