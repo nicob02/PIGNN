ac2de1fa147fcd3f1f528e04969f55003be52ee6
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-11-13 11:06:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ ic = func_main.init_condition
 dens = 70
 mesh = ElectrodeMesh(ru=(1, 1), lb=(0, 0), density=70)
 graph = mesh.getGraphData()
-model = msgPassing(message_passing_num=2, node_input_size=3+out_ndim, 
+model = msgPassing(message_passing_num=3, node_input_size=3+out_ndim, 
                    edge_input_size=3, ndim=out_ndim, device=device, model_dir=ckptpath)
 model.load_model(ckptpath)
 model.to(device)
