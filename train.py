3607b0493c71bcf12d8f8d19a02c8a20873ba78c
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-11-13 23:07:33 -0500

    Update train.py

--- train.py
+++ train.py
@@ -25,12 +25,12 @@ ic = func_main.init_condition
 bc1 = func_main.boundary_condition
 bc2 = func_main.electrode_condition
 
-model = msgPassing(message_passing_num=7, node_input_size=3+out_ndim, edge_input_size=3, 
+model = msgPassing(message_passing_num=1, node_input_size=3+out_ndim, edge_input_size=3, 
                    ndim=out_ndim, device=device, model_dir=ckptpath)    # Mess with MPN# to 2 or 3
 model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 
-mesh = ElectrodeMesh(ru=(0.7, 0.7), lb=(0.3, 0.3), density=35)
+mesh = ElectrodeMesh(ru=(0.65, 0.65), lb=(0.35, 0.35), density=35)
 
 graph = mesh.getGraphData().to(device)
 
