4a538d7c16535c6d53cf62e21b3df082ad294d05
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-11-13 20:49:58 -0500

    Update train.py

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ model = msgPassing(message_passing_num=1, node_input_size=3+out_ndim, edge_input
 model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 
-mesh = ElectrodeMesh(ru=(0.8, 0.8), lb=(0.2, 0.2), density=30)
+mesh = ElectrodeMesh(ru=(0.75, 0.75), lb=(0.25, 0.25), density=35)
 
 graph = mesh.getGraphData().to(device)
 
