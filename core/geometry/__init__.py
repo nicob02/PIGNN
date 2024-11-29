6fc9fd9351c2f6dbc4f9204170c338d7688fe888
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-11-28 20:33:38 -0500

    Update __init__.py

--- core/geometry/__init__.py
+++ core/geometry/__init__.py
@@ -90,7 +90,7 @@ class ElectrodeMesh():
         # Refine the mesh around marked cells
         initial_mesh = refine(initial_mesh, cell_markers)
         self.mesh = initial_mesh
-        self.pos = self.mesh.coordinates().astype(np.float32)
+        self.pos = self.mesh.coordinates().astype(np.float32).requires_grad_()
         self.faces = self.mesh.cells().astype(np.int64).T        
         self.node_type = get_node_type(self.pos, lb_electrode, ru_electrode).astype(np.int64)
         print("Node numbers: %d"%self.pos.shape[0])
