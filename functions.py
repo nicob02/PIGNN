38692292453622d1af46b57e1a5e16c09a071fa3
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-12-19 15:00:13 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -38,7 +38,7 @@ class ElectroThermalFunc():
 
     def init_condition(self, pos):
         
-        temp = torch.full_like(pos[:, 0:1], 0.310)  # Create a tensor filled with 310s for the temperature
+        temp = torch.full_like(pos[:, 0:1], 310)  # Create a tensor filled with 310s for the temperature
         volt = torch.full_like(pos[:, 1:2], 0)    # Create a tensor filled with 1s for the voltage
         #q =  torch.full_like(pos[:, 1:2], 0)      #q
         
@@ -47,7 +47,7 @@ class ElectroThermalFunc():
 
     def boundary_condition(self, pos):
         
-        temp = torch.full_like(pos[:, 0:1], 0.310)  # Create a tensor filled with 310s for the temperature
+        temp = torch.full_like(pos[:, 0:1], 310)  # Create a tensor filled with 310s for the temperature
         volt = torch.full_like(pos[:, 1:2], 0)    # Create a tensor filled with 1s for the voltage
     
         return torch.cat((temp, volt), dim=-1)    # Concatenate along the last dimension
@@ -123,7 +123,7 @@ class ElectroThermalFunc():
         if torch.isnan(squared_abs_grad_v).any():
             print("Warning: NaN detected in squared_abs_grad_v!")
             
-        sigma = f*(1+g*(temp_this*1000 - e)) # Sigma at t+1
+        sigma = f*(1+g*(temp_this - e)) # Sigma at t+1
         q = sigma*squared_abs_grad_v    # q at t+1
 
         lap_value = self.laplacianop(graph,values_this)
@@ -148,7 +148,7 @@ class ElectroThermalFunc():
         print(dvdt)
         print("lap_temp")
         print(lap_temp)
-        loss_temp = (0.001*((a*b*dvdt) - q - c*lap_temp -d*(e-temp_this*1000)))
+        loss_temp = (0.01*((a*b*dvdt) - q - c*lap_temp -d*(e-temp_this)))
         #loss_temp = (0.01*((a*b*dvdt) - q - c*lap_temp))
                      
         print("losses_tempthen_volt")
