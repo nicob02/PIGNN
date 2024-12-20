c9fd2fd73dd5925015af4ad5da91b69ad52149e2
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-12-19 19:40:17 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -148,7 +148,7 @@ class ElectroThermalFunc():
         print(dvdt)
         print("lap_temp")
         print(lap_temp)
-        loss_temp = (0.01*((a*b*dvdt) - q - c*lap_temp -d*(e-temp_this)))
+        loss_temp = (0.1*((a*b*dvdt) - q - c*lap_temp -d*(e-temp_this)))
         #loss_temp = (0.01*((a*b*dvdt) - q - c*lap_temp))
                      
         print("losses_tempthen_volt")
