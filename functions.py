a3910abbc4c721b07c34828b7ecc9a0e950b73c3
nicob02 <66340166+nicob02@users.noreply.github.com>
2024-12-20 12:57:51 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -38,19 +38,17 @@ class ElectroThermalFunc():
 
     def init_condition(self, pos):
         
-        temp = torch.full_like(pos[:, 0:1], 37)  # Create a tensor filled with 310s for the temperature
-        volt = torch.full_like(pos[:, 1:2], 0.5)    # Create a tensor filled with 1s for the voltage
+        volt = torch.full_like(pos[:, 0:1], 0.5)    # Create a tensor filled with 1s for the voltage
         #q =  torch.full_like(pos[:, 1:2], 0)      #q
         
-        return torch.cat((temp, volt), dim=-1)    # Concatenate along the last dimension
+        return torch.cat((volt), dim=-1)    # Concatenate along the last dimension
 
 
     def boundary_condition(self, pos):
         
-        temp = torch.full_like(pos[:, 0:1], 37)  # Create a tensor filled with 310s for the temperature
-        volt = torch.full_like(pos[:, 1:2], 0.2)    # Create a tensor filled with 1s for the voltage
-    
-        return torch.cat((temp, volt), dim=-1)    # Concatenate along the last dimension
+        volt = torch.full_like(pos[:, 0:1], 0.2)  # Create a tensor filled with 310s for the temperature
+
+        return torch.cat((volt), dim=-1)    # Concatenate along the last dimension
     
     def electrode_condition(self, pos, values_last, t):
         # Introduce a time-varying sinusoidal voltage source
@@ -60,10 +58,9 @@ class ElectroThermalFunc():
         # Voltage: V(t) = V0 * sin(2 * pi * f * t)
         time_var_volt = self.volt * math.sin(2 * torch.pi * rf_frequency * t)
        
-        temp = values_last[:,0:1]
-        #volt = torch.full_like(pos[:, 1:2], time_var_volt)    # Create a tensor filled with input voltage source
-        volt = torch.full_like(pos[:, 1:2], self.volt)    # Create a tensor filled with input voltage source
-        return torch.cat((temp, volt), dim=-1)
+        volt = torch.full_like(pos[:, 0:1], time_var_volt)    # Create a tensor filled with input voltage source
+        #volt = torch.full_like(pos[:, 1:2], self.volt)    # Create a tensor filled with input voltage source
+        return torch.cat((volt), dim=-1)
         
     def compute_gradient(self, field, positions):
 
@@ -92,24 +89,12 @@ class ElectroThermalFunc():
 
         a,b,c,d,e,f,g = self.params
               
-        temp_last = values_last[:,0:1]
-        volt_last = values_last[:,1:2]
-        temp_this = values_this[:,0:1]
-        volt_this = values_this[:,1:2]
+        volt_last = values_last[:,0:1]
+        volt_this = values_this[:,0:1]
 
-
-        if torch.isnan(temp_this).any() or torch.isnan(volt_this).any():
-            print("Warning: NaN detected in temp_this or volt_this before clamping!")
-    
         epsilon = 1e-4
   
-        if torch.isnan(values_this).any() or torch.isnan(values_last).any():
-            print("Warning: NaN detected in values_this or values_last after clamping!")
 
-        dvdt = (temp_this-temp_last)/self.delta_t
-            
-        if torch.isnan(dvdt).any():
-            print("Warning: NaN detected in dvdt!")
             
         grad_value = self.gradop(graph, values_this)
         grad_v = grad_value[1]          # Volt Gradient at t+1
@@ -117,48 +102,27 @@ class ElectroThermalFunc():
         print(graph.pos)
         print("graph.x")
         print(graph.x)
-        #grad_v = self.compute_gradient(volt_this, graph.pos)
+    
         squared_abs_grad_v = torch.sum(grad_v ** 2, dim=1, keepdim=True)  # Shape (N, 1)
-
-        if torch.isnan(squared_abs_grad_v).any():
-            print("Warning: NaN detected in squared_abs_grad_v!")
             
         sigma = f*(1+g*(temp_this - e)) # Sigma at t+1
-        q = sigma*squared_abs_grad_v    # q at t+1
 
-        lap_value = self.laplacianop(graph,values_this)
+        lap_value = self.laplacianop(graph,volt_this)
     
-        lap_temp = lap_value[:,0:1]
-        lap_volt = lap_value[:,1:2]
-        #grad_t = self.compute_gradient(temp_this, graph.pos)
-     
-        #lap_temp = self.compute_laplacian(grad_t, graph.pos).requires_grad_()  # ∇ · ∇T
-        #lap_volt = self.compute_laplacian(grad_v, graph.pos).requires_grad_()  # ∇ · ∇v
-        
-        if torch.isnan(lap_temp).any() or torch.isnan(lap_volt).any():
-            print("Warning: NaN detected in lap_temp or lap_volt!")
+        lap_volt = lap_value[:,0:1]
 
         #∇ · (σ(T)∇v) = 0
         loss_volt = sigma*lap_volt
-        #loss_volt = f*lap_volt
+ 
         print("lap_volt")
         print(lap_volt)
-        #ρticti*∂T/∂t = Q + ∇ · (d∇T) + H(Tbl − T), when H=0 we have the weak-formulation
-        print("dvdt")
-        print(dvdt)
-        print("lap_temp")
-        print(lap_temp)
-        loss_temp = (0.0001*((a*b*dvdt) - q - c*lap_temp -d*(e-temp_this)))
-        #loss_temp = (0.01*((a*b*dvdt) - q - c*lap_temp))
+      
                      
         print("losses_tempthen_volt")
-        print(loss_temp)
         print(loss_volt)
             
-
-
-        #return loss_volt
-        return torch.cat([loss_temp,loss_volt],axis=1)
+        return loss_volt
+        
 
     
     
