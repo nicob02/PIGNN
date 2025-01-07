e59bca09544382ddb1747ea3057b1b7f335d0b27
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-01-07 13:13:00 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import math
 class ElectroThermalFunc(): 
 
     func_name = 'rfa'
-    def __init__(self, delta_t, params, volt=None) -> None:
+    def __init__(self, delta_t, params) -> None:
         self.delta_t = delta_t
         self.params = params
         self.laplacianop = laplacian()
@@ -21,58 +21,49 @@ class ElectroThermalFunc():
         freq = self.params
 
         f = -2*freq*torch.sin(freq*x)*torch.sin(freq*y)
-        graph.x = torch.cat((graph.x,f), dim=-1)    # Append new Q value at t to the input
+        graph.x = torch.cat((graph.x,f), dim=-1)    # Append source f(x,y) value to the input
 
         return graph    
 
     def init_condition(self, pos):
         
-        volt = torch.full_like(pos[:, 0:1], 0.2)    # Create a tensor filled with 1s for the voltage
-        #q =  torch.full_like(pos[:, 1:2], 0)      #q
-        
-        return volt # Concatenate along the last dimension
+        volt = torch.full_like(pos[:, 0:1], 0)    # Create a tensor filled with 0s for the voltage
 
+        return volt 
 
     def boundary_condition(self, pos):
         
-        volt = torch.full_like(pos[:, 0:1], 0.2)  # Create a tensor filled with 310s for the temperature
+        volt = torch.full_like(pos[:, 0:1], 0)  # Create a tensor filled with 0s for the B.C. voltage
 
-        return volt    # Concatenate along the last dimension
+        return volt   
     
 
     def pde(self, graph, values_last, values_this, **argv):
 
-        a,b,c,d,e,f,g = self.params
+        x = graph.pos[:, 0:1]
+        y = graph.pos[:, 1:2]
+        freq = self.params        # w = angular frequency
               
         volt_last = values_last[:,0:1]
         volt_this = values_this[:,0:1]
 
-        epsilon = 1e-4
-  
-
-            
-        grad_value = self.gradop(graph, values_this)
-        grad_v = grad_value[0]          # Volt Gradient at t+1
         print("graph.pos")
         print(graph.pos)
         print("graph.x")
         print(graph.x)
-    
-        squared_abs_grad_v = torch.sum(grad_v ** 2, dim=1, keepdim=True)  # Shape (N, 1)
-            
-        sigma = f
 
         lap_value = self.laplacianop(graph,volt_this)
     
         lap_volt = lap_value[:,0:1]
 
-        #∇ · (σ(T)∇v) = 0
-        loss_volt = sigma*lap_volt
+        f = -2*freq*torch.sin(freq*x)*torch.sin(freq*y)
+        
+        #2w^2*sin(w*x)*sin(wy) - ∇ · (∇v) = 0
+        loss_volt = f - lap_volt
  
         print("lap_volt")
         print(lap_volt)
       
-                     
         print("losses_volt")
         print(loss_volt)
             
