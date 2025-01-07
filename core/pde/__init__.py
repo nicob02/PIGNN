f02e77b6178fffe92db81fce879c3162871fd22d
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-01-07 11:26:55 -0500

    Update __init__.py

--- core/pde/__init__.py
+++ core/pde/__init__.py
@@ -3,30 +3,16 @@ from .laplacian import SolveWeightLST2d
 # from .ic_bc import InitialCondition, BoundaryCondition
 import torch
 from torch_geometric.utils import get_laplacian
-from .cotangent_laplacian import CotangentLaplacian
+
 
 class laplacian():
     def __init__(self) -> None:
         self.L_matrix_dict = {}
-        #self.solver2d = SolveWeightLST2d()
-        self.cotlap = CotangentLaplacian()
-        
+        self.solver2d = SolveWeightLST2d()
+    
     def __call__(self, graph, values):
         
         L = self.L_matrix_dict.get(graph.label, None)
-        if L is None:
-            # Now we compute the laplacian using the cotangent method
-            # Ensure graph.face is available and correct
-            if not hasattr(graph, 'face'):
-                raise ValueError("Cotangent Laplacian requires 'face' attribute in graph")
-
-            # Construct a Data object with pos and face
-            data = graph
-            data.faces = graph.face.T  # If face is [3, F] and we need [F, 3]
-
-            L = self.cotlap(data)  # Get cotangent Laplacian
-            self.L_matrix_dict.setdefault(graph.label, L)
-        """
         if L is None:
             weights_t = self.solver2d(data=graph)
                 
@@ -35,7 +21,6 @@ class laplacian():
             edges, weight = get_laplacian(index, weights_t)
             L= torch.sparse.FloatTensor(edges, weight)
             self.L_matrix_dict.setdefault(graph.label, L)
-        """
             
         return -L.mm(values)
     
