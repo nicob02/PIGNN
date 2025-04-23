d1f1cd7ee8c714f344b6320f02953310a46e3242
Nicolas Bosteels <z5562856@ad.unsw.edu.au>
2025-04-23 13:07:32 -0400

    Revert "Update functions.py"
    This reverts commit 356062fab50604fe0c3468b8aa7c8347db60b744.

--- functions.py
+++ functions.py
@@ -38,11 +38,9 @@ class ElectroThermalFunc():
 
     def init_condition(self, pos):
         
-        # Generate random voltage values ~ N(1, 1)
-        volt = torch.randn_like(pos[:, 0:1]) + 1.0
-        
-        return volt
+        volt = torch.full_like(pos[:, 0:1], 1.5)    # Create a tensor filled with 0s for the voltage
 
+        return volt 
 
     def boundary_condition(self, graph, predicted):
         
